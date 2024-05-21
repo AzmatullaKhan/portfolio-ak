3f6fcf2d8b96df891222dd86e9d4598843ed9e9f
klu_2200031618 <kk258786@gmail.com>
2024-05-21 20:33:09 +0530

    Completed adding second project, removed own logic and added AOS library for animation

---
+++ src/pages/Project2.js
@@ -0,0 +1,24 @@
+export const Project2=()=>{
+    return(
+        <>
+            <h1 className="project-head">Todo List</h1>
+            <p className="project-desc">
+                -This React To-Do List application empowers users to efficiently manage tasks, prioritize activities, 
+                and enhance productivity. Seamlessly crafted with React, it offers a responsive and intuitive user interface, 
+                enabling users to add, edit, and delete tasks effortlessly. With dynamic updating and intuitive design, this 
+                application streamlines task management, helping users stay organized and focused. Experience the power of 
+                React in task management with this versatile To-Do List'application.
+            </p>
+            <p className="project-technologies"><span>Used Technnologies</span>:-  Reactjs, CSS and HTML</p>
+            <a href="https://github.com/AzmatullaKhan/todo-list">👉Github👈</a>
+            <div className="project-images">
+                <div style={{display:"flex", overflow:"scroll"}}>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview2-img1.png')}><img src={require('../images/preview2-img1.png')} alt='preview1-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview2-img2.png')}><img src={require('../images/preview2-img2.png')} alt='preview2-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview2-img3.png')}><img src={require('../images/preview2-img3.png')} alt='preview3-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview2-img4.png')}><img src={require('../images/preview2-img4.png')} alt='preview4-img' className='project-each-image'/></a>
+                </div>
+            </div>
+        </>
+    )
+}
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,8 +1,15 @@
 import { useEffect, useState } from 'react';
 import './AllPages.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import { Project1 } from './Project1';
+import { Project2 } from './Project2';
+
 
 export const Main=()=>{
+  useEffect(()=>{
+    AOS.init({duration:2000})
+  }, [])
     const words = [
         'I am Azmatulla.',
         "I have completed some projects.",
@@ -47,25 +54,17 @@ export const Main=()=>{
       
         return <div className="main-word-animation-container"><h1>{part}</h1></div>;
       };
-
-      window.addEventListener('scroll',()=>{
-        let y_scroll=window.pageYOffset;
-        if(y_scroll>=450){
-          document.getElementById('main_background_two').className='main-background-two'
-        }
-        else{
-          document.getElementById('main_background_two').className='hidden'
-        }
-      })
-
     return(
         <div className='main-background'>
           <div className='main-background-one'>
               <WordAnimation/>
               <h1 className='main-background-one-name'>-Azmatulla Khan</h1>
           </div>
-          <div className='hidden' id='main_background_two'>
+          <div className='main-background-two' id='main_background_two' data-aos="fade-left" data-aos-offset="300">
             <Project1 />
+          </div>
+          <div className='main-background-three' id='main_background_three' data-aos="fade-right" data-aos-offset="300">
+            <Project2 />
           </div>
         </div>
     )
