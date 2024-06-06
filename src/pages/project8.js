c4b7f1b9108d8b003281286e7ad1f0c507d6a9d9
klu_2200031618 <kk258786@gmail.com>
2024-06-06 20:03:38 +0530

    Preview 7,8 folders added

--- src/pages/project8.js
+++ src/pages/project8.js
@@ -15,16 +15,16 @@ export const Project8=()=>{
             <a rel="noreferrer" target={"_blank"} href="https://github.com/AzmatullaKhan/CSEdge-Internship-Tasks">👉Github👈</a>
             <div className="project-images">
                 <div style={{display:"flex", overflow:"scroll"}}>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview8-img1.png')}><img src={require('../images/preview8-img1.png')} alt='preview1-img' className='project-each-image'/></a>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview8-img2.png')}><img src={require('../images/preview8-img2.png')} alt='preview2-img' className='project-each-image'/></a>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview8-img3.png')}><img src={require('../images/preview8-img3.png')} alt='preview3-img' className='project-each-image'/></a>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview8-img4.png')}><img src={require('../images/preview8-img4.png')} alt='preview4-img' className='project-each-image'/></a>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview8-img5.png')}><img src={require('../images/preview8-img5.png')} alt='preview5-img' className='project-each-image'/></a>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview8-img6.png')}><img src={require('../images/preview8-img6.png')} alt='preview6-img' className='project-each-image'/></a>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview8-img7.png')}><img src={require('../images/preview8-img7.png')} alt='preview7-img' className='project-each-image'/></a>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview8-img8.png')}><img src={require('../images/preview8-img8.png')} alt='preview8-img' className='project-each-image'/></a>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview8-img9.png')}><img src={require('../images/preview8-img9.png')} alt='preview9-img' className='project-each-image'/></a>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview8-img10.png')}><img src={require('../images/preview8-img10.png')} alt='preview10-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview8/preview8-img1.png')}><img src={require('../images/preview8/preview8-img1.png')} alt='preview1-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview8/preview8-img2.png')}><img src={require('../images/preview8/preview8-img2.png')} alt='preview2-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview8/preview8-img3.png')}><img src={require('../images/preview8/preview8-img3.png')} alt='preview3-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview8/preview8-img4.png')}><img src={require('../images/preview8/preview8-img4.png')} alt='preview4-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview8/preview8-img5.png')}><img src={require('../images/preview8/preview8-img5.png')} alt='preview5-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview8/preview8-img6.png')}><img src={require('../images/preview8/preview8-img6.png')} alt='preview6-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview8/preview8-img7.png')}><img src={require('../images/preview8/preview8-img7.png')} alt='preview7-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview8/preview8-img8.png')}><img src={require('../images/preview8/preview8-img8.png')} alt='preview8-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview8/preview8-img9.png')}><img src={require('../images/preview8/preview8-img9.png')} alt='preview9-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview8/preview8-img10.png')}><img src={require('../images/preview8/preview8-img10.png')} alt='preview10-img' className='project-each-image'/></a>
                 </div>
             </div>
         </>
