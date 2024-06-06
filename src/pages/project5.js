9c8f85ad0d7dbd9733ac031dd862d49c8e466a98
klu_2200031618 <kk258786@gmail.com>
2024-06-06 20:00:47 +0530

    preview 4,5,6 folders added

--- src/pages/project5.js
+++ src/pages/project5.js
@@ -11,9 +11,9 @@ export const Project5=()=>{
             <a rel="noreferrer" target={"_blank"} href="https://github.com/AzmatullaKhan/chatting-bot">👉Github👈</a>
             <div className="project-images">
                 <div style={{display:"flex", overflow:"scroll"}}>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview5-img1.png')}><img src={require('../images/preview5-img1.png')} alt='preview1-img' className='project-each-image'/></a>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview5-img2.png')}><img src={require('../images/preview5-img2.png')} alt='preview2-img' className='project-each-image'/></a>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview5-img3.png')}><img src={require('../images/preview5-img3.png')} alt='preview3-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview5/preview5-img1.png')}><img src={require('../images/preview5/preview5-img1.png')} alt='preview1-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview5/preview5-img2.png')}><img src={require('../images/preview5/preview5-img2.png')} alt='preview2-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview5/preview5-img3.png')}><img src={require('../images/preview5/preview5-img3.png')} alt='preview3-img' className='project-each-image'/></a>
                 </div>
             </div>
         </>
--- src/pages/project6.js
+++ src/pages/project6.js
@@ -14,14 +14,14 @@ export const Project6=()=>{
             <a rel="noreferrer" target={"_blank"} href="https://github.com/AzmatullaKhan/ATM">👉Github👈</a>
             <div className="project-images">
                 <div style={{display:"flex", overflow:"scroll"}}>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview6-img1.png')}><img src={require('../images/preview6-img1.png')} alt='preview1-img' className='project-each-image'/></a>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview6-img2.png')}><img src={require('../images/preview6-img2.png')} alt='preview2-img' className='project-each-image'/></a>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview6-img3.png')}><img src={require('../images/preview6-img3.png')} alt='preview3-img' className='project-each-image'/></a>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview6-img4.png')}><img src={require('../images/preview6-img4.png')} alt='preview4-img' className='project-each-image'/></a>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview6-img8.png')}><img src={require('../images/preview6-img8.png')} alt='preview8-img' className='project-each-image'/></a>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview6-img5.png')}><img src={require('../images/preview6-img5.png')} alt='preview5-img' className='project-each-image'/></a>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview6-img6.png')}><img src={require('../images/preview6-img6.png')} alt='preview6-img' className='project-each-image'/></a>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview6-img7.png')}><img src={require('../images/preview6-img7.png')} alt='preview7-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview6/preview6-img1.png')}><img src={require('../images/preview6/preview6-img1.png')} alt='preview1-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview6/preview6-img2.png')}><img src={require('../images/preview6/preview6-img2.png')} alt='preview2-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview6/preview6-img3.png')}><img src={require('../images/preview6/preview6-img3.png')} alt='preview3-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview6/preview6-img4.png')}><img src={require('../images/preview6/preview6-img4.png')} alt='preview4-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview6/preview6-img8.png')}><img src={require('../images/preview6/preview6-img8.png')} alt='preview8-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview6/preview6-img5.png')}><img src={require('../images/preview6/preview6-img5.png')} alt='preview5-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview6/preview6-img6.png')}><img src={require('../images/preview6/preview6-img6.png')} alt='preview6-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview6/preview6-img7.png')}><img src={require('../images/preview6/preview6-img7.png')} alt='preview7-img' className='project-each-image'/></a>
                 </div>
             </div>
         </>
