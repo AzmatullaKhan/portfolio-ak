22e728eeacb040f997fb59bdfd59701eab5fe337
klu_2200031618 <kk258786@gmail.com>
2024-06-06 19:57:35 +0530

    Preview 2,3 foders added

--- src/pages/Project2.js
+++ src/pages/Project2.js
@@ -13,10 +13,10 @@ export const Project2=()=>{
             <a rel="noreferrer" target={"_blank"} href="https://github.com/AzmatullaKhan/todo-list">👉Github👈</a>
             <div className="project-images">
                 <div style={{display:"flex", overflow:"scroll"}}>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview2-img1.png')}><img src={require('../images/preview2-img1.png')} alt='preview1-img' className='project-each-image'/></a>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview2-img2.png')}><img src={require('../images/preview2-img2.png')} alt='preview2-img' className='project-each-image'/></a>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview2-img3.png')}><img src={require('../images/preview2-img3.png')} alt='preview3-img' className='project-each-image'/></a>
-                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview2-img4.png')}><img src={require('../images/preview2-img4.png')} alt='preview4-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview2/preview2-img1.png')}><img src={require('../images/preview2/preview2-img1.png')} alt='preview1-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview2/preview2-img2.png')}><img src={require('../images/preview2/preview2-img2.png')} alt='preview2-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview2/preview2-img3.png')}><img src={require('../images/preview2/preview2-img3.png')} alt='preview3-img' className='project-each-image'/></a>
+                    <a rel="noreferrer" target={"_blank"} href={require('../images/preview2/preview2-img4.png')}><img src={require('../images/preview2/preview2-img4.png')} alt='preview4-img' className='project-each-image'/></a>
                 </div>
             </div>
         </>
